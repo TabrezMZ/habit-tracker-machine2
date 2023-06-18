28ce2f885c1dcac44a8f9b843f6c566e81f7d564
TabrezMZ <tabrez.mansuri4u@gmail.com>
2023-06-18 21:40:34 +0530

    commit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -20,30 +20,32 @@ export const LandingPage = () => {
         <h1>All habits</h1>
         <button onClick={()=>setFormDisplay(true)}>Add Habit</button>
         { formDisplay && <HabitForm setFormDisplay= {setFormDisplay}/> }
-        <ul>
-            {state.allHabits?.map((habit, index) => (
-                <>
-                <li style={{display : habit.isArchived && 'none'}} key={index} >
-                    <div className="header">
-                        <h4>  habit name :{habit?.name}</h4>
+       {
+        !formDisplay &&  <ul>
+        {state.allHabits?.map((habit, index) => (
+            <>
+            <li style={{display : habit.isArchived && 'none'}} key={index} >
+                <div className="header">
+                    <h4>  habit name :{habit?.name}</h4>
+                </div>
+                <p>Rpeat : {habit?.repeat}</p>
+                <p>Goal : {habit?.goal}</p>
+                <p>number of times :{habit?.Times}</p>
+                <p> startdate : {habit?.startDate}</p>
+                <p>End Date : {habit?.endDate}</p>
+                <div className="details">
+                    <div className='buttons'>
+                        <button style={{color: 'green'}} onClick={()=>habitDispatch({type : 'ARCHIVE-HABIT' , payload : habit.id}) }  >add archive</button>
+                        <button style={{color: 'blue'}} onClick={()=>habitDispatch({type : 'DELETE-HABIT' , payload : habit.id}) } >Delete</button>
+                        <button style={{color: 'gray'}} onClick={()=> editHabit(habit.id)} >edit</button>
                     </div>
-                    <p>Rpeat : {habit?.repeat}</p>
-                    <p>Goal : {habit?.goal}</p>
-                    <p>number of times :{habit?.Times}</p>
-                    <p> startdate : {habit?.startDate}</p>
-                    <p>End Date : {habit?.endDate}</p>
-                    <div className="details">
-                        <div className='buttons'>
-                            <button style={{color: 'green'}} onClick={()=>habitDispatch({type : 'ARCHIVE-HABIT' , payload : habit.id}) }  >add archive</button>
-                            <button style={{color: 'blue'}} onClick={()=>habitDispatch({type : 'DELETE-HABIT' , payload : habit.id}) } >Delete</button>
-                            <button style={{color: 'gray'}} onClick={()=> editHabit(habit.id)} >edit</button>
-                        </div>
-                    </div>
-                </li>  
-                <hr />
-                </>
-            ))}
-        </ul>
+                </div>
+            </li>  
+            <hr />
+            </>
+        ))}
+    </ul>
+       }
     </div>
 </div>
   );
